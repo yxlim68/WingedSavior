1b43b749b0045d17a6211ba35990ce12b0ed138a
aidilrx04 <mrcrayv14@gmail.com>
2024-06-28 10:50:13 +0800

    export necessary items

--- WingedSaviorWebApp/html/conn.js
+++ WingedSaviorWebApp/html/conn.js
@@ -1,10 +1,5 @@
-const BACKEND_URL = "http://127.0.0.1:5000";
+export const BACKEND_URL = "http://127.0.0.1:5000";
 
-function api(path) {
+export function api(path) {
   return BACKEND_URL + path;
 }
-
-export default {
-  BACKEND_URL,
-  api,
-};
