9aef0ad2ed449474168b4ba67c644533553bb109
aidilrx04 <mrcrayv14@gmail.com>
2024-07-01 16:16:37 +0800

    add debugging option

--- WingedSaviorWebApp/html/conn.js
+++ WingedSaviorWebApp/html/conn.js
@@ -1,4 +1,11 @@
-export const BACKEND_URL = "http://127.0.0.1:5000";
+export const DEBUG_VIDEO = true;
+
+export let BACKEND_URL;
+if (DEBUG_VIDEO) {
+  BACKEND_URL = "http://127.0.0.1:5000";
+} else {
+  BACKEND_URL = "http://127.0.0.1:8766";
+}
 
 export function api(path) {
   return BACKEND_URL + path;
