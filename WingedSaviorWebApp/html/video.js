610f23dd7c0e85cf256ec8d1ca73d17b14057b2a
aidilrx04 <mrcrayv14@gmail.com>
2024-07-06 03:04:53 +0800

    require project to view live feed

--- WingedSaviorWebApp/html/video.js
+++ WingedSaviorWebApp/html/video.js
@@ -1,17 +1,18 @@
-import { api } from "./conn.js";
+import { api, requiredProject } from "./conn.js";
 import "./jquery.min.js";
 
 const $videoSrc = $("#video-src");
 const $err = $("#errmodal");
+requiredProject(async (projectId) => {
+  try {
+    // check connection
+    const url = api("/video_feed?project=" + projectId);
+    await fetch(url);
 
-try {
-  // check connection
-  const url = api("/video_feed");
-  await fetch(url);
-
-  // if success set video src html
-  $videoSrc.prop("src", url);
-} catch (e) {
-  $err.show();
-  console.error(e);
-}
+    // if success set video src html
+    $videoSrc.prop("src", url);
+  } catch (e) {
+    $err.show();
+    console.error(e);
+  }
+});
