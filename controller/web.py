be34aac9e05119331ff4f9f5409aee7239506bd3
aidilrx04 <mrcrayv14@gmail.com>
2024-07-07 22:55:37 +0800

    feat: add background frame

--- controller/web.py
+++ controller/web.py
@@ -1,6 +1,7 @@
 import asyncio
 
 import sys, os
+import threading
 from flask import Flask
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
@@ -8,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from controller.util import log
 
 import controller.routes as routes
-from drone.video import video_bp
+from drone.video import start_video_thread, video_bp
 from drone.location import location_bp
 
 from flask_cors import CORS
@@ -20,9 +21,18 @@ app.register_blueprint(location_bp)
 
 CORS(app)
 
+@app.before_request
+def init_components():
+    app.before_request_funcs[None].remove(init_components)
+    
+    video_thread = threading.Thread(target=start_video_thread, daemon=True)
+    video_thread.start()
+
+
 def init_app():
     print(app.url_map)
     app.run(host="0.0.0.0", port=8766, debug=True)    
+
     
 
 if __name__ == '__main__':
--- drone/video.py
+++ drone/video.py
@@ -1,7 +1,9 @@
 import datetime
+import time
 import cv2
 from flask import Blueprint, Response, request
 from controller.db import db
+from controller.util import log as util_log
 from drone.config import DEBUG_VIDEO
 from drone.detection import detect_person
 from drone.location import get_location
@@ -16,7 +18,7 @@ if DEBUG_VIDEO:
 
 @video_bp.route("/video_feed")
 def video_feed():
-    
+    global latest_frame, project
     project_id = request.args.get('project')
     
         
@@ -41,46 +43,21 @@ def video_feed():
     
     # keep tracked objects ids to prevent spamming database with images
     tracked_objects = list()
+    
+    project = project_id
 
     # TODO: Send predicted image to all connected clients
     def generate():
         while True:
-            frame = get_frame()
             
-            results = model.track(frame, persist=True, classes=0, verbose=False)
-
-            person_detected = detect_person(results[0])
-            
-            
-
-            if person_detected:
-                output = results[0].plot() 
-            else:
-                output = frame
-
-            success, jpeg = cv2.imencode('.jpg', output)
-            
-            if person_detected:
-                boxes = results[0].boxes
-                print(tracked_objects)
-                for box in boxes:
-                    if not box:
-                        continue
-                    if box.id in tracked_objects:
-                        continue
-                    
-                    tracked_objects.append(box.id.item())
-                    
-                    # send notification
-                    upload_image(project_id, box.id, jpeg.tobytes(), box.conf.item())
-
-
-            if not success:
-                print('[video_feed] Frame Dropped')
+            if latest_frame is None:
+                time.sleep(0.1)
                 continue
+            
+            
 
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + latest_frame.tobytes() + b'\r\n\r\n')
 
     return Response(generate(),  mimetype="multipart/x-mixed-replace;boundary=frame")
 
@@ -108,3 +85,67 @@ def upload_image( project_id: int, id: int, image_bytes, conf: float):
         print(e)
         
 
+latest_frame = None
+tracked_objects = list()
+project = None
+video_start = False
+
+def start_video_thread():
+    global latest_frame, tracked_objects, project, video_start
+    
+    log = util_log('video')
+    
+    if DEBUG_VIDEO:
+        cap = cv2.VideoCapture(0)
+    
+    if not DEBUG_VIDEO:
+        from drone.tello import tello, tello_connect_if_not
+        
+        tello_connect_if_not()
+        tello.streamon()
+     
+    def get_frame():
+        if DEBUG_VIDEO:
+            _, frame = cap.read()
+            return frame
+        
+        frame = tello.get_frame_read().frame
+        return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    
+    while True:
+        # log('frame: ', latest_frame)
+        # log(f'project: {project}')
+        frame = get_frame()
+        
+        results = model.track(frame, persist=True, classes=0, verbose=False)
+
+        person_detected = detect_person(results[0])
+
+        if person_detected:
+            output = results[0].plot() 
+        else:
+            output = frame
+
+        success, jpeg = cv2.imencode('.jpg', output)
+        
+        if person_detected:
+            boxes = results[0].boxes
+            print(tracked_objects)
+            for box in boxes:
+                if not box:
+                    continue
+                if box.id in tracked_objects:
+                    continue
+                
+                tracked_objects.append(box.id.item())
+                
+                # send notification
+                if project:
+                    upload_image(project, box.id, jpeg.tobytes(), box.conf.item())
+
+
+        if not success:
+            print('[video_feed] Frame Dropped')
+            continue
+        latest_frame = jpeg
+        video_start = True
